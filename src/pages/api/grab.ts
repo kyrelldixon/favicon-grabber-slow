c9135ad18555d25078ad5fa533fbead8f3a8203c
Kyrell Dixon <kyrell.brown@gmail.com>
2022-10-03 17:03:34 -0500

    feat(api): handle fetching from missing URL

--- src/pages/api/grab.ts
+++ src/pages/api/grab.ts
@@ -59,19 +59,25 @@ export default async function grab(req: NextApiRequest, res: NextApiResponse) {
     return res.status(200).json(cachedIcon)
   }
 
-  const icon = await firstValueFrom(
-    parseFavicon(hostname, textFetcher, bufferFetcher),
-  )
-  const iconWithFullUrl = iconToFullUrl(icon, pageUrl)
+  try {
+    const icon = await firstValueFrom(
+      parseFavicon(hostname, textFetcher, bufferFetcher),
+    )
+    const iconWithFullUrl = iconToFullUrl(icon, pageUrl)
 
-  // I'm caching for a while since it's not likely that URLs
-  // will change for a long time
-  const MAX_AGE_SECONDS = 60 * 60 * 24 * 30 // 30 days
+    // I'm caching for a while since it's not likely that URLs
+    // will change for a long time
+    const MAX_AGE_SECONDS = 60 * 60 * 24 * 30 // 30 days
 
-  console.log('caching', { hostname, iconWithFullUrl, ...req.query })
-  await cache.set<Icon>(hostname, iconWithFullUrl, MAX_AGE_SECONDS)
+    console.log('caching', { hostname, iconWithFullUrl, ...req.query })
+    await cache.set<Icon>(hostname, iconWithFullUrl, MAX_AGE_SECONDS)
 
-  return res.status(200).json(iconWithFullUrl)
+    return res.status(200).json(iconWithFullUrl)
+  } catch (error) {
+    return res.status(400).json({
+      message: `Could not find favicon for ${hostname}. Does this site exist?`,
+    })
+  }
 }
 
 function resolveUrl(url: string, baseUrl: string) {
